9c18f29a7e0e89c74e1a9a12f70432c1625130af
Max <dev@anuitex.com>
2019-04-29 16:10:44 +0300

    modify add component

--- src/app/users-base/dialogs/add/add.component.ts
+++ src/app/users-base/dialogs/add/add.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import {FormControl, Validators, FormGroup, FormBuilder} from '@angular/forms';
 
 
 @Component({
@@ -9,11 +10,37 @@ import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 })
 export class AddComponent implements OnInit {
 
+  addForm: FormGroup;
+
   constructor(
-    public dialogRef: MatDialogRef<AddComponent>
-  ) { }
+
+    public dialogRef: MatDialogRef<AddComponent>,
+    @Inject(MAT_DIALOG_DATA) public data,
+    private formBuilder: FormBuilder,
+
+
+  ) {
+
+
+  }
+
 
   ngOnInit() {
+    this.addForm = this.formBuilder.group({
+
+      firstName: new FormControl(null, Validators.required),
+      lastName: new FormControl(null, Validators.required),
+      email: new FormControl(null, [
+          Validators.required,
+          Validators.email
+        ]),
+      passwrod: new FormControl(null, Validators.required)
+
+    });
+  }
+
+  submit(){
+    console.log(this.addForm);
   }
 
   close(): void{
